a3945726172df28fd48451784a9fbd27dcbc7697
rafaelarango <arangoramirezrafael@gmail.com>
2021-07-12 14:24:57 -0500

    Nuevo model orders.js

--- api/model/users.js
+++ api/model/users.js
@@ -5,38 +5,37 @@ module.exports = (sequelize, Sequelize) => {
       autoIncrement: true,
       primaryKey: true,
     },
-    id_rol:{
-        type: Sequelize.INTEGER,
-        allowNull: false,
+    id_rol: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
-    name:{
-        type: Sequelize.STRING,
-        allowNull: false,
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
     },
-    type_document:{
-        type: Sequelize.STRING,
-        allowNull: false,
+    type_document: {
+      type: Sequelize.STRING,
+      allowNull: false,
     },
-    document:{
-        type: Sequelize.STRING,
-        allowNull: false,
+    document: {
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     direction: {
-        type: Sequelize.STRING,
-        allowNull: false,
+      type: Sequelize.STRING,
+      allowNull: false,
     },
-    phone:{
-        type: Sequelize.STRING,
-        allowNull: false,
+    phone: {
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     email: {
-        type: Sequelize.STRING,
-        allowNull: false,
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    }
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   });
 };
-
